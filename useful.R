f7b03a97bdd2807e392004d00803f433c55ca1b4
Yushan-Gu <66291061+Yushan-Gu@users.noreply.github.com>
2024-05-02 18:21:24 -0400

    Update useful.R

--- useful.R
+++ useful.R
@@ -1,3 +1,25 @@
+asName <- function(name)
+{
+  N <- gsub(pattern = "[^A-Za-z0-9_]", 
+                 replacement = '.', 
+                 name)
+  sapply(N, function(l)if_else(grepl("[0-9]",substr(l,1,2)),
+                               paste0("X",l), l)) %>% return()
+  }
+
+asNameExp <- function(text){
+  Name <- ""
+  index <- gregexpr("`", text)[[1]] %>% as.vector()
+  Name <- paste0(Name,substr(text, 1,index[1]))
+  for (i in (1:(length(index)/2))*2-1) {
+    Name <- paste0(Name, substr(text,index[i]+1,index[i+1]-1) %>% asName(),"`")
+    if(index[i+1]!=str_length(text)){
+      Name <- paste0(Name, substr(text,index[i+1]+1,index[i+2]-1),"`")
+    }
+  }
+  Name %>% return()
+}
+
 replaceAll <- function(v, r, new){
   replace(v, v %in% r, new)
 }
